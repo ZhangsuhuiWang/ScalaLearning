8ee6d78fe90328859993feb608d4947fac5357b9
wang jerry <w18756276802@gmail.com>
2019-03-01 11:12:55 +0800

    chapter12 parallel

--- build.sbt
+++ build.sbt
@@ -3,3 +3,4 @@ name := "ScalaLearning"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
--- src/main/scala/Parallel/ShortCircuit.scala
+++ src/main/scala/Parallel/ShortCircuit.scala
@@ -1,1 +1,17 @@
+def expensiveComputation() = {
+  println("...assume slow operation...")
+  false
+}
+
+def evaluate(input: Int): Unit = {
+  println(s"evaluate called with $input")
+  lazy val perform = expensiveComputation()
+  if(input >= 10 && perform)
+    println("doing work...")
+  else
+    println("skipping")
+}
+
+evaluate(0)
+evaluate(10)
 
